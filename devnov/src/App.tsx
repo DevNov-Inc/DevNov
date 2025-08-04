70ce073b4fc907a486dabec6fe9bb6e0df48b0c7
Simcita <mlisanasimamnkele10@gmail>
2025-08-04 20:11:45 +0200

    fixing firebase errors

--- devnov/src/App.tsx
+++ devnov/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import {useEffect, useState} from 'react'
 import { Route, Routes, Navigate, useNavigate } from 'react-router-dom'
 import '../src/App.css'
 import Login from './components/login/login'
