ca4753ce6405fe6773fb74b257529f374c90eab1
LEVY315 <132851230+LEVY315@users.noreply.github.com>
2025-08-20 01:39:56 +0200

    Security tight

--- devnov/firebaseConfig.ts
+++ devnov/firebaseConfig.ts
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBbfkyNvoTPYVFKYweKFTyM0K3M-S4WOoc",
-  authDomain: "devnov-cdb9a.firebaseapp.com",
-  projectId: "devnov-cdb9a",
-  storageBucket: "devnov-cdb9a.firebasestorage.app",
-  messagingSenderId: "487843659530",
-  appId: "1:487843659530:web:95d9c38bb8e34dd9cd44f3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
--- devnov/src/APIconfigs/Users.ts
+++ devnov/src/APIconfigs/Users.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API_URL = "https://devback-ewem.onrender.com/api/users";
+const API_URL = import.meta.env.VITE_USERS+"/users";
 
 // User Type
 export type User = {
